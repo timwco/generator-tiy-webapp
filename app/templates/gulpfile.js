20f7f25b2ce177cd32d278d212ceec9359ac82f2
Tim Whitacre <tim@argyllstudios.com>
2014-07-05 20:27:30 -0400

    Updated the  task. Added a prompt and some confirm/error logs for better understanding of what is happening

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var rimraf = require('rimraf');
 var exec = require('child_process').exec;
+var prompt = require('gulp-prompt');
 
 gulp.task('styles', function () {<% if (includeSass) { %>
   return gulp.src('app/styles/main.scss')
@@ -111,7 +112,22 @@ gulp.task('default', ['clean'], function () {
 
 // Push a subtree from our `dist` folder
 gulp.task('deploy', function() {
-  exec('git subtree push --prefix dist origin gh-pages');
+
+  gulp.src('/')
+    .pipe(prompt.prompt({
+        type: 'confirm',
+        name: 'task',
+        message: 'This will deploy to GitHub Pages. Have you already built your application and pushed your updated master branch?'
+    }, function(res){
+      if (res.task){
+        console.log('Attempting: "git subtree push --prefix dist origin gh-pages"');
+        exec('git subtree push --prefix dist origin gh-pages', function(err, stdout, stderr) {
+            console.log(stdout);
+            console.log(stderr);
+        });
+      } else { console.log('Please do this first and then run `gulp deploy` again.'); }
+    }));
+
 });
 
 // Test your app in the browser
